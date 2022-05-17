f47c456b529d01e264fd2eda78ff112b98a688e0
uisheshnath <shesh212333@gmail.com>
2022-05-17 13:28:20 +0530

    hesh

---
+++ header.js
@@ -0,0 +1,4 @@
+const header = () =>
+{
+    document.write("This is testing for header....");
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+function f() {
+    var a;
+   
+    for (a=1;a<=100;a++) {
+        document.getElementsByClassName("demo").innerHTML = a;
+    }
+    
+    
+}
